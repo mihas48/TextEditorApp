cc0a5e050b06f4ab5b42716e0ab7fae06d2e79cf
mihas48 <danilka.mihaylov.2018@yandex.ru>
2026-03-13 11:21:55 +0300

    Add implementation of formatting buttons. Change size of text input block

--- TextEditorApp/MainWindow.xaml.cs
+++ TextEditorApp/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
+using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TextEditorApp
 {
@@ -19,6 +13,64 @@ namespace TextEditorApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        enum FormatingMode
+        {
+            bold, italic, underline
+        }
+
+        private void ToggleFormat(object buttonSender, DependencyProperty property, FormatingMode mode, object activeValue, object normalValue)
+        {
+            if (!rtb.Selection.IsEmpty)
+            {
+                rtb.Selection.ApplyPropertyValue(property, ((ToggleButton)buttonSender).IsChecked == true ? activeValue : normalValue);
+            }
+
+            else
+            {
+                switch (mode)
+                {
+                    case FormatingMode.bold:
+                        if (EditingCommands.ToggleBold.CanExecute(null, rtb))
+                        {
+                            EditingCommands.ToggleBold.Execute(null, rtb);
+                        }
+                        break;
+                    case FormatingMode.italic:
+                        if (EditingCommands.ToggleItalic.CanExecute(null, rtb))
+                        {
+                            EditingCommands.ToggleItalic.Execute(null, rtb);
+                        }
+                        break;
+                    case FormatingMode.underline:
+                        if (EditingCommands.ToggleUnderline.CanExecute(null, rtb))
+                        {
+                            EditingCommands.ToggleUnderline.Execute(null, rtb);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            //Возвращаем фокус в поле с текстом
+            rtb.Focus();
+        }
+
+        private void ToggleButtonBold_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleFormat(sender, TextElement.FontWeightProperty, FormatingMode.bold, FontWeights.Bold, FontWeights.Normal);
+        }
+
+        private void ToggleButtonItalic_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleFormat(sender, TextElement.FontStyleProperty, FormatingMode.italic, FontStyles.Italic, FontStyles.Normal);
+        }
+
+        private void ToggleButtonUnderline_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleFormat(sender, Inline.TextDecorationsProperty, FormatingMode.underline, TextDecorations.Underline, null);
         }
     }
 }
