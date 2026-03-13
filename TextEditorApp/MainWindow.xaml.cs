1433cfa6c3085ef49a974c56ee2edd46be207afb
mihas48 <danilka.mihaylov.2018@yandex.ru>
2026-03-13 16:53:49 +0300

    Add buttons for: formating text, formating paragraphs.

--- TextEditorApp/MainWindow.xaml.cs
+++ TextEditorApp/MainWindow.xaml.cs
@@ -1,23 +1,26 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 
 namespace TextEditorApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private List<ToggleButton> alignmentButtons;
         public MainWindow()
         {
             InitializeComponent();
+            alignmentButtons = new List<ToggleButton> {
+            togBtnStart,
+            togBtnCenter,
+            togBtnEnd,
+            togBtnJustify
+            };
         }
 
         enum FormatingMode
         {
-            bold, italic, underline
+            bold, italic, underline, subscript, superscript
         }
 
         private void ToggleFormat(object buttonSender, DependencyProperty property, FormatingMode mode, object activeValue, object normalValue)
@@ -58,6 +61,19 @@ namespace TextEditorApp
             rtb.Focus();
         }
 
+        private void ToggleVariant(object buttonSender, object activeValue, object normalValue)
+        {
+            if (!rtb.Selection.IsEmpty)
+            {
+                rtb.Selection.ApplyPropertyValue(Typography.VariantsProperty, ((ToggleButton)buttonSender).IsChecked == true ? activeValue : normalValue);
+            }
+            else
+            {
+                ((ToggleButton)buttonSender).IsChecked = !((ToggleButton)buttonSender).IsChecked;
+                MessageBox.Show("Сначала выделете текст", "Сообщение", MessageBoxButton.OK);
+            }
+        }
+
         private void ToggleButtonBold_Click(object sender, RoutedEventArgs e)
         {
             ToggleFormat(sender, TextElement.FontWeightProperty, FormatingMode.bold, FontWeights.Bold, FontWeights.Normal);
@@ -71,6 +87,75 @@ namespace TextEditorApp
         private void ToggleButtonUnderline_Click(object sender, RoutedEventArgs e)
         {
             ToggleFormat(sender, Inline.TextDecorationsProperty, FormatingMode.underline, TextDecorations.Underline, null);
+        }
+
+        private void ToggleButtonSubscript_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)togBtnSup.IsChecked)
+                togBtnSub.IsChecked = false;
+            ToggleVariant(sender, FontVariants.Subscript, FontVariants.Normal);
+        }
+
+        private void ToggleButtonSuperscript_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)togBtnSub.IsChecked)
+                togBtnSub.IsChecked = false;
+            ToggleVariant(sender, FontVariants.Superscript, FontVariants.Normal);
+        }
+
+        private void ToggleButtonAlignmentStart_Click(object sender, RoutedEventArgs e)
+        {
+            rtb.CaretPosition.Paragraph?.TextAlignment = TextAlignment.Left;
+            UnCheckAllAligmentFlags();
+            togBtnStart.IsChecked = true;
+        }
+
+        private void ToggleButtonAlignmentCenter_Click(object sender, RoutedEventArgs e)
+        {
+            rtb.CaretPosition.Paragraph?.TextAlignment = TextAlignment.Center;
+            UnCheckAllAligmentFlags();
+            togBtnCenter.IsChecked = true;
+        }
+
+        private void ToggleButtonAlignmentEnd_Click(object sender, RoutedEventArgs e)
+        {
+            rtb.CaretPosition.Paragraph?.TextAlignment = TextAlignment.Right;
+            UnCheckAllAligmentFlags();
+            togBtnEnd.IsChecked = true;
+        }
+
+        private void ToggleButtonAlignmentJustify_Click(object sender, RoutedEventArgs e)
+        {
+            rtb.CaretPosition.Paragraph?.TextAlignment = TextAlignment.Justify;
+            UnCheckAllAligmentFlags();
+            togBtnJustify.IsChecked = true;
+        }
+
+        private void UnCheckAllAligmentFlags()
+        {
+            foreach (var button in alignmentButtons)
+                button.IsChecked = false;
+        }
+        private void RichTextBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            UnCheckAllAligmentFlags();
+            switch (rtb.CaretPosition.Paragraph?.TextAlignment)
+            {
+                case TextAlignment.Left:
+                    togBtnStart.IsChecked = true;
+                    break;
+                case TextAlignment.Right:
+                    togBtnEnd.IsChecked = true;
+                    break;
+                case TextAlignment.Center:
+                    togBtnCenter.IsChecked = true;
+                    break;
+                case TextAlignment.Justify:
+                    togBtnJustify.IsChecked = true;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
